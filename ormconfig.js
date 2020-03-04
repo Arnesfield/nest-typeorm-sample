3a47f66ee6b252022b074c41574847fc20b66755
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-03-04 12:44:59 +0800

    Drop db schema even in development environment

--- ormconfig.js
+++ ormconfig.js
@@ -6,6 +6,6 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   synchronize: String(process.env.DB_SYNCHRONIZE).toLowerCase() === 'true',
-  dropSchema: process.env.NODE_ENV === 'test',
+  dropSchema: ['development', 'test'].includes(process.env.NODE_ENV),
   entities: ['dist/**/*.entity{.ts,.js}']
 };
