d0e3748fcb7963d9e68b98202df05c60d0bf4423
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-03-04 14:25:23 +0800

    Add World modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { WorldModule } from './world/world.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, WorldModule],
   controllers: [AppController],
   providers: [AppService]
 })
